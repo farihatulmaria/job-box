726bb00172f8377d679eb6e38e6dab64e23748b5
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2023-01-12 14:10:45 +0600

    some updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import './App.css';
-import logo from './logo.svg';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code className='text-red-500 text-5xl'>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
